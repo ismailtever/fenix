512098e913a0f20d801f730ac1a78e596703e698
ismailtever <113910333+ismailtever@users.noreply.github.com>
2023-12-02 16:10:28 +0300

    feat: model created

---
+++ fenix/Model/Movies.swift
@@ -0,0 +1,58 @@
+//
+//  Movies.swift
+//  fenix
+//
+//  Created by Ismail Tever on 2.12.2023.
+//
+
+import Foundation
+
+// MARK: - Movies
+struct Movies: Codable {
+    let page: Int?
+    let results: [Movie]?
+    let totalPages: Int?
+    let totalResults: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Movie
+struct Movie: Codable {
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIDS: [Int]?
+    let id: Int?
+    let originalLanguage: String?
+    let originalTitle: String?
+    let overview: String?
+    let popularity: Double?
+    let posterPath: String?
+    let releaseDate: String?
+    let title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview
+        case popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title
+        case video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
